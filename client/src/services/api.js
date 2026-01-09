016ef95fe24bc89773696d7604a90f92f0745d95
JahnaviJagSan <jahnavijaiswal0@gmail.com>
2026-01-09 13:05:47 +0530

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+if (!API_URL) {
+  console.error('VITE_API_URL is missing!');
+}
 // Create axios instance with default config
 const api = axios.create({
   baseURL: API_URL,
